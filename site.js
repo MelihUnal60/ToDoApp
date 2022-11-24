7d0d8e284c0c415d4ed3edd807e95588c0847026
MelihUnal60 <melih.unal@uyumsoft.com>
2022-11-24 11:40:35 +0300

    ToDoApp

--- site.js
+++ site.js
@@ -14,16 +14,47 @@ function eventListener(){
     from.addEventListener("submit",addNewItem);
 
     //delete an item
+    taskList.addEventListener('click',deleteItem);
+    
     //delete all item
     btnDeleteAll.addEventListener("click",deleteAllItem);
+
+    //edit item
+    taskList.addEventListener('click',editItem);
 }
 function deleteAllItem(e){
 
     if(confirm("Are you sure?")){
+        // silmek için ilk yöntem
         taskList.innerHTML='';
+
+        //silmek için 2.yöntem
+        // while(taskList.firstChild){
+        //     taskList.removeChild(taskList.firstChild);
+        // }
     }
 
     e.preventDefault();
+}
+
+function deleteItem(e){
+    //console.log(e);
+    if(e.target.className==="bi bi-x-circle"){
+        //console.log("i elementine tıklandı!!");
+        if(confirm("Are you sure ?")){
+            //console.log(e);
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function editItem(e){
+
+    
+    if(e.target.className==="btn btn-secondary btn-sm float-end mx-3"){
+        input.value.innerText = e.target.parentElement.innerText;
+    }
+    
 }
 
 function addNewItem(e){
@@ -38,7 +69,7 @@ function addNewItem(e){
     }
 
     const li=document.createElement('li');
-    li.classList='list-group list-group-item-secondary'; //li elementi oluşturduk
+    li.classList='list-group-item list-group-item-secondary'; //li elementi oluşturduk
     li.appendChild(document.createTextNode(input.value));
 
     const a=document.createElement('a');
@@ -46,8 +77,16 @@ function addNewItem(e){
     a.setAttribute('href','#');
     a.innerHTML='<i class="bi bi-x-circle"></i>';
 
+    const edit=document.createElement('btn');
+    edit.classList='btn btn-secondary btn-sm float-end mx-3';
+    edit.innerHTML='Edit';
+
+
     li.appendChild(a); //a yı li nin içine soktuk
+    li.appendChild(edit);
     taskList.appendChild(li); //li yi task listin child yaptık
+
+
 
 
 }
